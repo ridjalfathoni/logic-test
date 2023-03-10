9f4b4706cfc2412145f5af94ccd41b4e38c4bd2a
ridjalfathoni <fathonirosyidi@gmail.com>
2023-03-10 12:42:41 +0700

    soal2

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -27,9 +29,52 @@ func soal1() {
 	}
 
 	fmt.Println("  " + strings.Repeat("- ", len(x)))
-	fmt.Print(xAxis)
+	fmt.Println(xAxis)
+}
+
+func hitungTipeData(jsonData interface{}, count map[string]int) {
+	switch tipeKey := jsonData.(type) {
+	case string:
+		count["string"]++
+	case bool:
+		count["boolean"]++
+	case int, float64:
+		count["number"]++
+	case map[string]interface{}:
+		for _, key := range tipeKey {
+			hitungTipeData(key, count)
+		}
+	}
+}
+
+func soal2() {
+	jsonFile, err := ioutil.ReadFile("mardy.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var jsonData map[string]interface{}
+
+	err = json.Unmarshal(jsonFile, &jsonData)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// jsonDataStr, _ := json.Marshal(jsonData)
+	// fmt.Println(string(jsonDataStr))
+
+	totalOutput := map[string]int{"string": 0, "boolean": 0, "number": 0}
+	for _, key := range jsonData {
+		hitungTipeData(key, totalOutput)
+	}
+
+	fmt.Println("string:", totalOutput["string"])
+	fmt.Println("boolean:", totalOutput["boolean"])
+	fmt.Println("number:", totalOutput["number"])
 }
 
 func main() {
+	fmt.Println("============ Soal 1 ============")
 	soal1()
+	fmt.Println("============ Soal 2 ============")
+	soal2()
 }
